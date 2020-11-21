dc5f1254f81a494ffc026fefde5c4a9b5638f20c
atmatawaran <atmatawaran@up.edu.ph>
2020-11-21 22:48:22 +0800

    - loading infomarkers

--- src/components/Map.js
+++ src/components/Map.js
@@ -29,6 +29,7 @@ export default function Map(){
 
     const [facilities,setFacilities] = useState([]);
     const [markers, setMarkers] = useState([]);
+    const [selected, setSelected] = useState(null);
 
     useEffect(() =>{
         db.collection("facilities").onSnapshot(function(data){
@@ -78,8 +79,18 @@ export default function Map(){
             key={facility.fac_id}
             position={new window.google.maps.LatLng(parseFloat(facility.fac_gps.latitude),parseFloat(facility.fac_gps.longitude))}
             onLoad={onLoad}
+            onClick={() => setSelected(facility)}
           />
         ))}
+
+        {selected ? (<InfoWindow position={new window.google.maps.LatLng(parseFloat(selected.fac_gps.latitude),parseFloat(selected.fac_gps.longitude))}
+                    onCloseClick={() => {
+                      setSelected(null);
+                    }}>
+          <div>
+            <h6>{selected.fac_id}</h6>
+          </div>
+        </InfoWindow>): null}
       
       </GoogleMap>
     </div>
